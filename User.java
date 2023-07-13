76efdcddd9200c7747da57b52b7889383dc1020e
alexpbb <133996219+alexpbb@users.noreply.github.com>
2023-07-13 13:42:42 -0400

    Add files via upload

--- User.java
+++ User.java
@@ -1,20 +1,28 @@
 public class User {
-    private String fName;
-    private String lName;
-    private String sex;
+    private String name;
     private int age;
-    private int height;
+    private double height;
     private double weight;
+    private boolean loseWeight;
 
-    public User(String fName, String lName, String sex, int age, int height, double weight) {
-        this.fName = fName;
-        this.lName= lName;
-        this.sex = sex;
+    public User(String name, int age, double height, double weight, boolean loseWeight) {
+        this.name = name;
         this.age = age;
         this.height = height;
         this.weight = weight;
+        this.loseWeight = loseWeight;
     }
 
-    
-
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public double getWeight() {
+        return weight;
+    }
 }
